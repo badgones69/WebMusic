ed3c897ec1a45756861871812b78221d9adbbbd7
Florian Renard <florian.renard86@gmail.com>
2018-03-21 12:50:57 +0100

    SQLite connection creation

---
+++ src/main/java/database/SQLiteConnection.java
@@ -0,0 +1,33 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteConnection {
+
+    public static void connect() {
+
+        Connection connection = null;
+
+        try {
+            // URL to the database file
+            String url = "jdbc:sqlite:C:/workspace/WebMusic.db";
+
+            // Creation of a connection to the database
+            connection = DriverManager.getConnection(url);
+
+            System.out.println("Connection to your SQLite database established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
