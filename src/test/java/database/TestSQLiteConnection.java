2924eceb0c03644b80e9750a31e1b08699a77219
Florian Renard <florian.renard86@gmail.com>
2019-07-24 10:39:20 +0200

    Useless logs generation deleting correction

--- src/test/java/database/TestSQLiteConnection.java
+++ src/test/java/database/TestSQLiteConnection.java
@@ -1,35 +1,14 @@
 package database;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestSQLiteConnection {
 
-    private OutputStream outputStream;
-
-    @Before
-    public void setUp() throws Exception {
-        outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
-        System.setOut(printStream);
-    }
-
     @Test
     public void connect() {
         SQLiteConnection.connect();
-        assertEquals("", outputStream.toString());
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        PrintStream originalOut = System.out;
-        System.setOut(originalOut);
+        assertEquals(Boolean.TRUE, SQLiteConnection.isConnectionEstablished());
     }
 }
